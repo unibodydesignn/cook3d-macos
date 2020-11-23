19e54d5a573d8b4c384b4335ad480572d75524e0
unibodydesignn <unibodydesignn@gmail.com>
2020-11-23 23:03:17 +0300

    Initial Commit

---
+++ Cook3D/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Cook3D
+//
+//  Created by Melih Mete on 23.11.2020.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Cook3D/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Cook3D
+//
+//  Created by Melih Mete on 23.11.2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
