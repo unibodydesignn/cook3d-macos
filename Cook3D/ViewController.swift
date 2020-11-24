41ca62b223df3a8c37a6b6408e42033e66c29376
unibodydesignn <unibodydesignn@gmail.com>
2020-11-24 20:30:53 +0300

    Initial commit

--- Cook3D/ViewController.swift
+++ Cook3D/ViewController.swift
@@ -6,12 +6,14 @@
 //
 
 import Cocoa
+import Metal
+import MetalKit
 
 class ViewController: NSViewController {
 
+    var metalView: NSView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -21,6 +23,7 @@ class ViewController: NSViewController {
         }
     }
 
-
+    
+        
 }
 
